c84d9ad10a6bb962f92d6a2f0d5e189ea5b5cc60
puralmajor <71024217+puralmajor@users.noreply.github.com>
2021-06-03 21:53:27 +0900

    2차

--- login_with_registration/signup-check.php
+++ login_with_registration/signup-check.php
@@ -50,7 +50,7 @@ if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['name']) && 
             header("Location: signup.php?error=이미 존재하는 ID입니다.&$user_data");
             exit();
         }else{
-            $sql2 = "INSERT INTO prison_officer(PO_id,PO_name,PO_rank,PO_password,PRISON_BUILDING_PB_id) VALUES('$id', '$name', '$rank', '$password', '$building')";
+            $sql2 = "INSERT INTO prison_officer(PO_id,PO_name,PO_rank,PO_password,PRISON_BUILDING_PB_id) VALUES('$id', '$name', '$rank', '$pass', '$building')";
             $result2 = mysqli_query($conn,$sql2);
             if($result2) {
                 echo "<script:javascript> alert(\"등록완료\');</script>";
